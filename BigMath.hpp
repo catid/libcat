221e86764bbd476e5a861e66fd72e105f25e0730
Christopher A. Taylor <chris@gameclosure.com>
2014-03-02 19:57:08 -0800

    updated API

--- BigMath.hpp
+++ BigMath.hpp
@@ -43,35 +43,59 @@ namespace cat {
 
 #ifndef CAT_HAS_U128
 	struct u128 {
-		u64 hi, lo;
+		u64 lo, hi;
 	};
 #endif
 
 // r = x
+// The high part is set to zero
 CAT_INLINE void u128_set(u128 &r, const u64 x);
+
+// r = high : low
+CAT_INLINE void u128_set(u128 &r, const u64 low, const u64 high);
 
 // x + y
 CAT_INLINE u128 u128_sum(const u64 x, const u64 y);
+CAT_INLINE u128 u128_sum(const u128 x, const u128 y);
 
 // x - y
 CAT_INLINE u128 u128_diff(const u64 x, const u64 y);
+CAT_INLINE u128 u128_diff(const u128 x, const u128 y);
 
 // r += x
 CAT_INLINE void u128_add(u128 &r, const u128 x);
 CAT_INLINE void u128_add(u128 &r, const u64 x);
 
-// r = u128_high(r) + x
-// Negative r values are not sign-extended
-CAT_INLINE void u128_add_carry(u128 &r, const u64 x);
-
-// r = (r >> 64) + x
-// Note that the difference here is that r is signed,
-// so negative values are sign-extended
-CAT_INLINE void u128_add_borrow(u128 &r, const u64 x);
+// r = -r
+CAT_INLINE u128 u128_neg(const u128 x);
 
 // r -= x
 CAT_INLINE void u128_sub(u128 &r, const u128 x);
 CAT_INLINE void u128_sub(u128 &r, const u64 x);
+
+// r |= x
+CAT_INLINE void u128_or(u128 &r, const u64 x);
+
+// r = u128_high(r) + x
+// Negative r values are not sign-extended
+CAT_INLINE void u128_carry_add(u128 &r, const u64 x);
+
+CAT_INLINE void u128_carry_add(u128 &r, const u64 x, const u64 y)
+{
+	u128_carry_add(r, x);
+	u128_add(r, y);
+}
+
+// r = (r >> 64) + x
+// Note that the difference here is that r is signed,
+// so negative values are sign-extended
+CAT_INLINE void u128_borrow_add(u128 &r, const u64 x);
+
+CAT_INLINE void u128_borrow_add_sub(u128 &r, const u64 x, const u64 y)
+{
+	u128_borrow_add(r, x);
+	u128_sub(r, y);
+}
 
 // r <<= shift
 CAT_INLINE void u128_lshift(u128 &r, int shift);
@@ -82,12 +106,18 @@ CAT_INLINE u128 u128_prod(const u64 x, const u64 y);
 // x * y + z
 CAT_INLINE u128 u128_prod_sum(const u64 x, const u64 y, const u64 z);
 
+// (x << shift) + z
+CAT_INLINE u128 u128_lshift_sum(const u64 x, int shift, const u64 z);
+
 // x * y assuming MSB(x) = MSB(y) = 0
 CAT_INLINE u128 u128_prod_63(const u64 x, const u64 y);
 
 // get high/low halves
 CAT_INLINE u64 u128_high(const u128 x);
 CAT_INLINE u64 u128_low(const u128 x);
+
+// Clear MSB of parameter
+CAT_INLINE void u128_clear_msb(u128 &r);
 
 
 /*
